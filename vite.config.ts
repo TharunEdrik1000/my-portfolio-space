79aea684b7f668221bb0a83c498e52107d47420e
TharunEdrik1000 <tharunedrik2006@gmail.com>
2026-03-19 21:06:36 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    export default defineConfig({
+  base: "/portfolio/",
+})
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
